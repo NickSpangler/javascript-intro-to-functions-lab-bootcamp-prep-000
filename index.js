6d71b6b3b605a0d0b243e1008dc5a6323c76bdac
Nick Spangler <spangleyman@mac.com>
2020-05-01 20:34:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if string.toUpperCase() === string.ToUpperCase()
+  if string.toLowerCase() === string.ToUpperCase()
   return ()
 }
